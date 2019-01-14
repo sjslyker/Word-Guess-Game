7cc9e9300e930fa0134ee2930d5a45c38a9ffd4a
Steven S <stevenjslyker@gmail.com>
2019-01-13 22:40:00 -0700

    FinalVersion

--- Javascript/game.js
+++ Javascript/game.js
@@ -1,28 +1,103 @@
-
-var marioCharacters = ["mario", "luigi", "bowser", "peach", "warrio", "waluigi", "toad", "yoshi", "goomba", "KoopaTroopa", "toadette", "daisy", "birdo", "koopa", "KingBoo", "DonkeyKong"];
-    
-var currentWord ="";
-
-var cWrdLttrs ="";
-
-var blanks="";
-
-var wrongLetters="";
-
-
-
 var wins = 0;
 var losses = 0;
-var guessesLeft = 6;
+var game = function () {
+    newGame();
+}
 
-console.log("test")
-document.write("HELLLOOOOOOOOOOOOOO")
+var newGame = function () {
+    var marioCharacters = ["mario", "luigi", "bowser", "peach", "warrio", "waluigi", "toad", "yoshi", "goomba", "koopatroopa", "toadette", "daisy", "birdo", "koopa", "kingboo", "donkeykong"];
+        
+    var currentWord = marioCharacters[Math.floor(Math.random() * marioCharacters.length)];
 
-function newGame() {
-    currentWord = marioCharacters[Math.floor(Math.random() * marioCharacters.length)];
-    console.log('Current Word: ' + currentWord);
+    var cWrdLttrs = currentWord.split("");
 
-    currentWord = currentWord.split("");
-    console.log('Current Word Letters:' + currentWord);
+    var rightLetters = [];
+
+    var wrongLetters = [];
+
+    var attempts = 7;
+
+    var letterCount = cWrdLttrs.length;
+
+    var rightGuessLetterCount = 0;
+    
+    for (var i = 0; i < currentWord.length; i++) {
+        rightLetters.push("_ ");
+        document.getElementById("theWord").innerHTML = rightLetters.join("");
+        document.getElementById("remLives").innerHTML = attempts;
+        document.getElementById("lettersGuessed").innerHTML = wrongLetters;
+    }
+    document.onkeyup = function(event) {
+        var playerGuess = event.key;
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            if (cWrdLttrs.indexOf(playerGuess) > -1) {
+                for (var i = 0; i < letterCount; i++) {
+                    if (cWrdLttrs[i] === playerGuess) {
+                        rightLetters[i] = playerGuess;
+                        document.getElementById("theWord").innerHTML = rightLetters.join("");
+                        rightGuessLetterCount++;
+                    }
+                }    
+            }
+            else {
+                attempts--;
+                wrongLetters.push(playerGuess);
+                document.getElementById("remLives").innerHTML = attempts;
+                document.getElementById("lettersGuessed").innerHTML = wrongLetters;
+                    if (attempts <= 6) {
+                        document.getElementById("mPuMario").style.opacity = "0.1";
+                    }
+                    if (attempts <= 5) {
+                        document.getElementById("mStar").style.opacity = "0.1";
+                    }
+            
+                    if (attempts <= 4) {
+                        document.getElementById("mRgMario").style.opacity = "0.1";
+                    }
+            
+                    if (attempts <= 3) {
+                        document.getElementById("mCoin").style.opacity = "0.1";
+                    }
+            
+                    if (attempts <= 2) {
+                        document.getElementById("mSmMario").style.opacity = "0.1";
+                    }
+            
+                    if (attempts <= 1) {
+                        document.getElementById("mSmMario").style.opacity = "0.1";
+                    }
+            
+                    if (attempts <= 0) {
+                        document.getElementById("mPuMario").style.opacity = ".99";
+                        document.getElementById("mStar").style.opacity = ".99";
+                        document.getElementById("mRgMario").style.opacity = ".99";
+                        document.getElementById("mCoin").style.opacity = ".99";
+                        document.getElementById("mSmMario").style.opacity = ".99";
+                        document.getElementById("mPeach").style.opacity = ".99";
+                        newGame();
+                    }
+    
+            }
+
+            if (rightGuessLetterCount === cWrdLttrs.length) {
+                wins++;
+                document.getElementById("wins").innerHTML = wins;
+                alert("You Win! Word: " + cWrdLttrs.join(''));
+                game();
+            }
+            if (attempts === 0) {
+                losses++;
+                document.getElementById("losses").innerHTML = losses;
+                alert("You Lost! Word: " + cWrdLttrs.join(''));
+                game();
+            }
+        }  
+        else {
+            alert("Please select a letter from the Alphabet (a to z)");
+        }  
+    }
 
 }
+
+
+newGame();
